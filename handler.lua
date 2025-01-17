610bbb9e4b8794fbac99f1bc78ee7004d889c8e1
NITIN GAIKWAD <ngaikwda@gmail.com>
2025-01-17 10:08:17 +0530

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -1,24 +1,24 @@
 local ngx = ngx
 local kong = kong
-local openssl_pkey = require("openssl.pkey")
+local openssl_pkey = require("resty.openssl.pkey")
 
-local EncryptionPlugin = {}
+local RSAEncPlugin = {}
 
 -- Constructor
-function EncryptionPlugin:new()
+function RSAEncPlugin:new()
   local obj = {}
   setmetatable(obj, self)
   self.__index = self
   return obj
 end
 
--- Header filter phase to remove Content-Length header
-function EncryptionPlugin:header_filter()
+-- Header filter phase to prepare the body for encryption
+function RSAEncPlugin:header_filter()
   ngx.header["Content-Length"] = nil
 end
 
 -- Body filter phase to encrypt the response body
-function EncryptionPlugin:body_filter(conf)
+function RSAEncPlugin:body_filter(conf)
   local chunk = ngx.arg[1]
   local eof = ngx.arg[2]
 
@@ -32,14 +32,13 @@ function EncryptionPlugin:body_filter(conf)
   end
 
   if eof then
-    local public_key = conf.public_key
-    if not public_key then
+    if not conf.public_key then
       kong.log.err("Public key not configured in the plugin")
       ngx.arg[1] = ngx.ctx.buffer -- Send the original response if encryption fails
       return
     end
 
-    local pkey, err = openssl_pkey.new(public_key)
+    local pkey, err = openssl_pkey.new(conf.public_key)
     if not pkey then
       kong.log.err("Failed to load public key: ", err)
       ngx.arg[1] = ngx.ctx.buffer -- Send the original response if encryption fails
@@ -58,7 +57,7 @@ function EncryptionPlugin:body_filter(conf)
 end
 
 -- Define the plugin priority and version
-EncryptionPlugin.PRIORITY = 10
-EncryptionPlugin.VERSION = "1.0.0"
+RSAEncPlugin.PRIORITY = 10
+RSAEncPlugin.VERSION = "1.0.0"
 
-return EncryptionPlugin
+return RSAEncPlugin
